0af3eb35c8cc1f91042abfd7420471c1f3e7c599
Your Name <you@example.com>
2022-11-25 12:27:03 +0600

    added user info in collection

--- index.js
+++ index.js
@@ -12,13 +12,26 @@ app.use(express.json())
 
 
 const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@cluster0.p2qoups.mongodb.net/?retryWrites=true&w=majority`;
-// console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
 
 async function run() {
   try {
-    const userCollection = client.db("laptopLab").collection("user");
+    const usersCollection = client.db("laptopLab").collection("users");
+
+  // user info insert
+  app.put("/user/:email", async (req, res) => {
+        const email = req.params.email;
+        const user = req.body;
+        const filter = { email: email };
+        const options = { upsert: true };
+        const updateDoc = {
+            $set: user
+        }
+        const result = await usersCollection.updateOne(filter, updateDoc, options);
+        res.send(result)
+     })
+
+
   }
   finally {
   }
