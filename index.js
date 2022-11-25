45f8a6a39a95b8fa39680be05fceaf354f5b88f1
Your Name <you@example.com>
2022-11-25 10:36:38 +0600

    added mongo connected

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
 
 const app = express()
@@ -10,10 +11,14 @@ app.use(cors())
 app.use(express.json())
 
 
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@cluster0.p2qoups.mongodb.net/?retryWrites=true&w=majority`;
+// console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
 
 async function run() {
   try {
-    
+    const userCollection = client.db("laptopLab").collection("user");
   }
   finally {
   }
